d6ece44424bbf43e254def0bc2acc1be624997d9
Pratham Khandelwal <pratham.khandelwal@advarisk.com>
2025-05-14 00:48:58 +0530

    agent initial setup

---
+++ main.py
@@ -0,0 +1,56 @@
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent
+from tools import search_tool
+
+load_dotenv()
+
+class ResearchResponse(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+
+
+llm = ChatAnthropic(model="claude-3-5-sonnet-20241022")
+parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+
+prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                """
+                You are a research assistant that will help generate a research paper.
+                Answer the user query and use necessary tools.
+                Wrap the output in this format and provide no other text\n{format_instructions}
+                """,
+                ),
+                ("placeholder", "{chat_history}"),
+                ("human", "{query}"),
+                ("placeholder", "{agent_scratchpad}"),
+            ]
+        ).partial(format_instructions=parser.get_format_instructions())
+
+tools = [search_tool]
+agent = create_tool_calling_agent(
+        LLm=llm,
+        prompt=prompt,
+        tooLs=[]
+        )        
+
+agent_executor = AgnetExecutor(agent=agent, tools, verbose=True)
+query = input("")
+raw_response = agent_executor.invoke({"query": "What is the capital of France?"})
+print(raw_response)
+try:
+    structured_response = parser.parse(raw_response.get("output")[0]["text"])
+    print(structured_response)
+except Exception as e:
+    print("Error parsing response", e, "Raw Response - ", raw_response)
+
+#resp = llm.invoke("What is meaning of working passionately and hardly in a IT startup. How to grow?")
+#print(resp)
